29e5409c7146acd4f7acdaf08ab4e895ae69901d
t-noahadash <t-noahadash@microsoft.com>
2023-05-26 16:28:41 +0300

    .

--- ex3_316279264/message_reader.c
+++ ex3_316279264/message_reader.c
@@ -1,9 +1,9 @@
 #include <fcntl.h>
-// #include <unistd.h>
-// #include <sys/ioctl.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "message_slot.h"
 
